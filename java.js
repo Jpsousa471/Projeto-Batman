3646f72b1a3648e279e730c526a79c81f365987d
João Pedro <jpsousa471@gmail.com>
2024-08-28 23:32:26 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+// Cookies & LocalStorage & SessionStorage //
+
+// Salvando dados no LocalSotorage 
+window.localStorage.setItem("Projeto", "Curintia")
+// Pegando o item salvo
+console.log(window.localStorage.getItem("name"));
